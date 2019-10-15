44f0914c7b502f77b0ed7031d239787c8fc52d70
karthickasit <karthickasme@gmail.com>
2019-10-15 13:02:16 +0530

    bootstrap

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_04_072119) do
+ActiveRecord::Schema.define(version: 2019_10_15_043929) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "bname"
